bedd2f3c0f7b21f55bd744dbdffbe9187abdb9b1
= <=>
2024-09-27 13:39:34 -0300

    começo

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import LayoutFull from '../layouts/LayoutFull.vue';
 import CrudJogador from '../views/CrudJogador.vue';
 import CrudTime from '../views/CrudTime.vue'
 import TestComponents from '@/views/TestComponents.vue';
-
+import Tabela from '@/views/Tabela.vue';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -35,6 +35,11 @@ const router = createRouter({
       name : '',
       component : TestComponents
     },
+    {
+      path : '/test',
+      name : '',
+      component : Tabela
+    }
   ],
 });
 
---
+++ src/views/Tabela.vue
@@ -0,0 +1,71 @@
+<script setup></script>
+
+
+<template>
+<div>
+    <div class="tabela">
+        <table>
+        <thead>
+            <tr>
+      <th scope="col">Classificação</th>
+      <th scope="col">P</th>
+      <th scope="col">J</th>
+      <th scope="col">V</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>1</td>
+      <td>22</td>
+      <td>33</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>1</td>
+      <td>45</td>
+      <td>21</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>1</td>
+      <td>29</td>
+      <td>77</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>1</td>
+      <td>36</td>
+      <td>43</td>
+    </tr>
+    <tr>
+      <th scope="row">1</th>
+      <td>1</td>
+      <td>43</td>
+      <td>99</td>
+    </tr>
+    
+  </tbody>
+    </table>
+    </div>
+</div>
+
+</template>
+
+
+<style>
+table {
+    border-collapse: collapse;
+    border: 2px solid rgb(0, 0, 0);
+    font-family: sans-serif;
+    font-size: 0.8rem;
+    letter-spacing: 1px;
+
+  }
+
+  th,
+  td {
+    border: 1px solid rgb(160 160 160);
+    padding: 8px 10px;
+  } 
+  </style>
