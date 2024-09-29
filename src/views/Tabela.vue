251a7c98d69944fa5d4700aa21030c9878aec6b1
= <=>
2024-09-29 17:37:25 -0300

    começo tabela

--- src/views/Tabela.vue
+++ src/views/Tabela.vue
@@ -1,71 +1,75 @@
-<script setup></script>
-
+<script setup>
+const times = [
+  { classificacao: 'Vermelho', P: '1', J: '2', V: '2' },
+  { classificacao: 'Preto', P: '12', J: '4', V: '5' },
+  { classificacao: 'Branco', P: '3', J: '9', V: '7' },
+  { classificacao: 'Rosa', P: '7', J: '6', V: '4' },
+  { classificacao: 'Verde', P: '1', J: '2', V: '3' },
+  { classificacao: 'Rosa', P: '7', J: '6', V: '4' },
+  { classificacao: 'Verde', P: '1', J: '2', V: '3' },
+];
+</script>
 
 <template>
-<div>
-    <div class="tabela">
-        <table>
-        <thead>
-            <tr>
-      <th scope="col">Classificação</th>
-      <th scope="col">P</th>
-      <th scope="col">J</th>
-      <th scope="col">V</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>1</td>
-      <td>22</td>
-      <td>33</td>
-    </tr>
-    <tr>
-      <th scope="row">1</th>
-      <td>1</td>
-      <td>45</td>
-      <td>21</td>
-    </tr>
-    <tr>
-      <th scope="row">1</th>
-      <td>1</td>
-      <td>29</td>
-      <td>77</td>
-    </tr>
-    <tr>
-      <th scope="row">1</th>
-      <td>1</td>
-      <td>36</td>
-      <td>43</td>
-    </tr>
-    <tr>
-      <th scope="row">1</th>
-      <td>1</td>
-      <td>43</td>
-      <td>99</td>
-    </tr>
-    
-  </tbody>
+  <div class="tabela-container">
+    <table class="tabela">
+      <thead>
+        <tr>
+          <th scope="col">Classificação</th>
+          <th scope="col">P</th>
+          <th scope="col">J</th>
+          <th scope="col">V</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(time, index) in times" :key="index">
+          <th scope="row">{{ time.classificacao }}</th>
+          <td>{{ time.P }}</td>
+          <td>{{ time.J }}</td>
+          <td>{{ time.V }}</td>
+        </tr>
+      </tbody>
     </table>
-    </div>
-</div>
-
+  </div>
 </template>
 
-
 <style>
+.tabela-container {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
+
 table {
-    border-collapse: collapse;
-    border: 2px solid rgb(0, 0, 0);
-    font-family: sans-serif;
-    font-size: 0.8rem;
-    letter-spacing: 1px;
+  border-collapse: separate;
+  border-spacing: 0;
+  width: 971px;
+  height: 683px;
+  font-family: sans-serif;
+  font-size: 1rem;
+  letter-spacing: 1px;
+  background-color: #1E1E1E;
+  color: #FFFFFF;
+  border: 2px solid rgb(160, 160, 160);
+  border-radius: 15px;
+}
 
+
+th,
+td {
+  border: 1px solid #757575;
+  padding: 15px 15px;
+  text-align: center;
+  font-size: 32px;
+}
+
+.tabela-container{
+  background-color: black;
+  width: 1066px;
+  height: 803px;
+  align-items: center;
+  border-radius: 10px;
+  margin-left: 700px;
+  border-radius: 15px; 
   }
-
-  th,
-  td {
-    border: 1px solid rgb(160 160 160);
-    padding: 8px 10px;
-  } 
-  </style>
+</style>
--- src/views/TestComponents.vue
+++ src/views/TestComponents.vue
@@ -1,43 +1,8 @@
 <script setup></script>
 <template>
-    <div class="header">
-      <div class="logo">
-        <span >COPÃO</span>
-      </div>
-      <nav class="nav-links">
-        <a href="#" class="nav-item" @click="setActive('classificacao')">📊 Classificação</a>
-        <a href="#" class="nav-item" @click="setActive('chaveamento')">📅 Chaveamento</a>
-        <a href="#" class="nav-item" @click="setActive('jogos')">⚽ Jogos</a>
-      </nav>
-    </div>
+
+  a
   </template>
   
   <style scoped>
-.header {
-  display: flex;
-  background-color: #333;
-  padding: 25px 20px;
-  align-items: center;
-}
-
-.logo {
-  font-size: 24px;
-  font-weight: bold;
-  color: white;
-}
-
-.nav-links {
-  display: flex;
-  gap: 20px;
-  margin-left: 80px;
-}
-
-.nav-item {
-  color: white;
-  text-decoration: none;
-  font-size: 16px;
-  position: relative;
-  padding-bottom: 5px;
-}
-
 </style>
