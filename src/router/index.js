6c602ba86fe4ea93448c8b4b67442d5b209cfd43
= <=>
2024-10-02 14:47:49 -0300

    refact

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import CrudJogador from '../views/CrudJogador.vue';
 import CrudTime from '../views/CrudTime.vue'
 import TestComponents from '@/views/TestComponents.vue';
 import Tabela from '@/views/Tabela.vue';
+import TestCard from '@/views/TestCard.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
