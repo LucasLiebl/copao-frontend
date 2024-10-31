91b1442c3e732b6030f3b5b906c9cb137352b3b0
= <=>
2024-10-31 16:23:12 -0300

    refact tabela

--- src/components/TabelaComponent.vue
+++ src/components/TabelaComponent.vue
@@ -1,81 +1,83 @@
-<script setup>
+  <script setup>
+import { computed } from 'vue';
 
-const props = defineProps({
-  times:{
-    type: Object
+  const props = defineProps({
+    times:{
+      type: Object
+    }
+  })
+
+  const sortedTimes = computed(() => {
+  return [...props.times].sort((a, b) => b.pontos - a.pontos); // Ordena em ordem decrescente de pontos
+});
+  </script>
+
+  <template>
+    <div class="tabela-container">
+      <table class="tabela">
+          <thead>
+          <tr>
+            <th scope="col">Classificação</th>
+            <th scope="col">P</th>
+            <th scope="col">J</th>
+            <th scope="col">V</th>
+            <th scope="col">E</th>
+            <th scope="col">D</th>
+            <th scope="col">GP</th>
+            <th scope="col">GC</th>
+            <th scope="col">%</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(time, index) in sortedTimes" :key="index">
+            <th scope="row">{{ time.nome }}</th>
+            <td>{{ time.pontos }}</td>
+            <td>{{ time.vitoria + time.empate + time.derrota }}</td>
+            <td>{{ time.vitoria }}</td>
+            <td>{{ time.empate }}</td>
+            <td>{{ time.derrota }}</td>
+            <td>{{ time.gols_pro }}</td>
+            <td>{{ time.gols_contra }}</td>
+            <td>{{ (time.vitoria/(time.vitoria + time.empate + time.derrota) * 100).toFixed(0)  }} </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
+
+  <style>
+
+  table {
+    border-collapse: separate;
+    border-spacing: 0;
+    width: 971px;
+    height: 683px;
+    font-family: sans-serif;
+    font-size: 1rem;
+    letter-spacing: 1px;
+    background-color: #1E1E1E;
+    color: #FFFFFF;
+    border: 5px solid #757575;
+    border-radius: 15px;
   }
-})
-</script>
-
-<template>
-  <div class="tabela-container">
-    <table class="tabela">
-        <thead>
-        <tr>
-          <th scope="col">Classificação</th>
-          <th scope="col">P</th>
-          <th scope="col">J</th>
-          <th scope="col">V</th>
-          <th scope="col">E</th>
-          <th scope="col">D</th>
-          <th scope="col">GP</th>
-          <th scope="col">GC</th>
-          <th scope="col">%</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(time, index) in props.times" :key="index">
-          <th scope="row">TIME</th>
-          <td>{{ time.pontos }}</td>
-          <td>{{ time.vitoria + time.empate + time.derrota }}</td>
-          <td>{{ time.vitoria }}</td>
-          <td>{{ time.empate }}</td>
-          <td>{{ time.derrota }}</td>
-          <td>{{ time.gols_pro }}</td>
-          <td>{{ time.gols_contra }}</td>
-          <td>{{ (time.vitoria/(time.vitoria + time.empate + time.derrota) * 100).toFixed(0)  }} </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-</template>
-
-<style>
-
-table {
-  border-collapse: separate;
-  border-spacing: 0;
-  width: 971px;
-  height: 683px;
-  font-family: sans-serif;
-  font-size: 1rem;
-  letter-spacing: 1px;
-  background-color: #1E1E1E;
-  color: #FFFFFF;
-  border: 5px solid #757575;
-  border-radius: 15px;
-}
 
 
-th,
-td {
-  border: 1px solid #757575;
-  padding: 15px 15px;
-  text-align: center;
-  font-size: 32px;
-  font-weight: 600;
-}
-th{
-
-}
-.tabela-container{
-  display: flex;
-  justify-content: center;
-  background-color: #1E1E1E;
-  width: 1066px;
-  height: 803px;
-  align-items: center;
-  border-radius: 10px;
-  border-radius: 15px; 
+  th,
+  td {
+    border: 1px solid #757575;
+    padding: 15px 15px;
+    text-align: center;
+    font-size: 32px;
+    font-weight: 600;
   }
-</style>
+  .tabela-container{
+    display: flex;
+    justify-content: center;
+    background-color: #1E1E1E;
+    width: 1066px;
+    height: 803px;
+    align-items: center;
+    border-radius: 10px;
+    border-radius: 15px; 
+    }
+  </style>
