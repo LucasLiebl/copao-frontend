aa53b6da8d380162824d39b237d1bc1e0fb946c2
1montandon <emontandon11@gmail.com>
2024-11-16 04:01:13 -0300

    No crud jogo, apresenta cada gol feito, com opcao de deletar

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.s6tosjqpob8"
+    "revision": "0.lugmo9h60ag"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
