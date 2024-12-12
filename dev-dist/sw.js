5a934e983790ecce1094d32a317cda027453d891
= <=>
2024-12-11 21:43:32 -0300

    Arrumando css do crudJogador

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,11 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
+<<<<<<< HEAD
     "revision": "0.ljht0nup4lo"
+=======
+    "revision": "0.l82pahrf9to"
+>>>>>>> 5fe96dc (Arrumando css do crudJogador)
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
